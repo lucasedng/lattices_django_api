1d56d6527632c983ee81bbcc44617d1ad5b791ae
lucasedng <lucasedng@gmail.com>
2022-01-07 13:22:05 -0300

    procfile and requirements update to deploy on hrk

--- api_lattices/settings.py
+++ api_lattices/settings.py
@@ -12,7 +12,9 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 from dotenv import load_dotenv
+import django_heroku
 
+django_heroku.settings(locals())
 load_dotenv()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
